00b00d695141296d738fda976fe7a584890347f4
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2021-02-15 13:30:42 +0200

    handle the inflate of the layout design of add new fragment class

--- app/src/main/java/com/a7medelnoor/mynote/fragments/AddNewNoteFragment.kt
+++ app/src/main/java/com/a7medelnoor/mynote/fragments/AddNewNoteFragment.kt
@@ -1,12 +1,14 @@
 package com.a7medelnoor.mynote.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.a7medelnoor.mynote.MainActivity
 import com.a7medelnoor.mynote.R
 import com.a7medelnoor.mynote.databinding.FragmentHomeBinding
+import com.a7medelnoor.mynote.viewmodel.NoteViewModel
 
 /***
  *  Note Android Application
@@ -14,12 +16,15 @@ import com.a7medelnoor.mynote.databinding.FragmentHomeBinding
  *  www.github.com/a7medelnoor
  */
 class AddNewNoteFragment : Fragment(R.layout.fragment_add_new_note) {
-    private var _bindingAddNewFragment : FragmentHomeBinding? =null
+    private var _bindingAddNewFragment: FragmentHomeBinding? = null
     private val bindingAddNewFragment get() = _bindingAddNewFragment!!
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var mView: View
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        noteViewModel = (activity as MainActivity).noteViewModel
+        mView = view
     }
 
     override fun onCreateView(
@@ -35,4 +40,8 @@ class AddNewNoteFragment : Fragment(R.layout.fragment_add_new_note) {
         return bindingAddNewFragment.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _bindingAddNewFragment = null
+    }
 }
